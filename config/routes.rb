3bd3fd23b4d7d265257175f7df058f5970dae664
punn1035 <punnipa602.41@gmail.com>
2018-07-03 10:40:14 +0700

    Add hp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'homepages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :homepages
 end
